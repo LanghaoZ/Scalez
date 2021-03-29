2f4a86148ec89ffa0c9b33a2f40d07ef7263cda1
Langhao Zhong <zhong74.langhao@hotmail.com>
2021-03-29 03:18:32 -0700

    Configured Remote MySQL Instance

--- sql/init.sql
+++ sql/init.sql
@@ -37,8 +37,8 @@ CREATE TABLE `Order` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `User` VALUES(3105007261, "Langhao", "123456", "da4Kj32k");
-INSERT INTO `Product` VALUES(1, "iPad Pro", "Apple's latest generation iPad Pro", 799.99, 10000);
-INSERT INTO `Product` VALUES(2, "MacBook Pro", "Apple's latest generation Macbook", 2199.99, 10000);
+INSERT INTO `User` VALUES(3105007261, "Langhao", "a1cf50cb0cd7ac425d2ecd0dc05f7dc3", "da4Kj32k");
+INSERT INTO `Product` VALUES(1, "iPad Pro", "Apple's latest generation iPad Pro", 799.99, 1000);
+INSERT INTO `Product` VALUES(2, "MacBook Pro", "Apple's latest generation Macbook", 2199.99, 1000);
 
 
